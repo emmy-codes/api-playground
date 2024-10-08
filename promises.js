17b81ee1394b0f6fd7f09656df6efc51c1fddfa7
Emma <alunia.moon@gmail.com>
2024-10-08 15:50:24 +0200

    Swapped promise function for async try/catch

--- promises.js
+++ promises.js
@@ -1,18 +1,29 @@
 const throwButton = document.getElementById("throwPokeball").addEventListener("click", () => {
-    pokemonCatch(2);
+    asyncFn(6);
 })
 
-const pokemonCatch = (id) => {
+// const pokemonCatch = (id) => {
 
-    fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
-        .then((response) => {
-            if (!response.ok) {
-                throw new Error("Network response error")
-            }
-            return response.json();
-        })
-        .then((data) => {
-            const pokemonImg = document.getElementById("pokemonImgDisplay");
-            pokemonImg.src = data.sprites.front_default;
-        })
+//     fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+//         .then((response) => {
+//             if (!response.ok) {
+//                 throw new Error("Network response error")
+//             }
+//             return response.json();
+//         })
+//         .then((data) => {
+//             const pokemonImg = document.getElementById("pokemonImgDisplay");
+//             pokemonImg.src = data.sprites.front_default;
+//         })
+// };
+
+const asyncFn = async (id) => {
+    try {
+        const apiResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+        const pokemonImg = document.getElementById("pokemonImgDisplay");
+        const data = await apiResponse.json();
+        pokemonImg.src = data.sprites.front_default;
+    } catch (error) {
+        console.log("Network response error");
+    };
 };
